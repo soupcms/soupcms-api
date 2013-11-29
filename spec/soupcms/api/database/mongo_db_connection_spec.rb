e573166b160e3d2b64940feca8fc7b0476fcb210
Sunit Parekh <parekh.sunit@gmail.com>
2013-11-29 21:56:55 +0530

    commit to trigger travis-ci build

--- spec/soupcms/api/database/mongo_db_connection_spec.rb
+++ spec/soupcms/api/database/mongo_db_connection_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Verify MongoDB Connection' do
 
   it 'should verify simple mongodb connection' do
-    expect(SoupCMS::Api::MongoDbConnection.new('local').conn.database_names.size).to be > 0
+    expect(SoupCMS::Api::MongoDbConnection.new('local').conn.database_names.size).to be > 1
   end
 
 
