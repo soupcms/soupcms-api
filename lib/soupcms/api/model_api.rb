b5030a1e1fdfba378571b6416ad98aff3c0cb363
Sunit Parekh <parekh.sunit@gmail.com>
2013-12-01 17:32:51 +0530

    refactored controller using grape api group feature

--- lib/soupcms/api/model_api.rb
+++ lib/soupcms/api/model_api.rb
@@ -7,30 +7,39 @@ module SoupCMS
     prefix 'api'
     format :json
 
-    desc 'get published documents'
-    params do
-      optional :tags, type: Array, default: []
-      optional :include, type: String, default: 'published'
-    end
-    get ':app_name/:model_name', requirements: { app_name: /[A-Za-z0-9\-]*/, model_name: /[A-Za-z0-9]*/ } do
-      service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
-      params['include'] == 'published' ? service_model.published : service_model.drafts
-      service_model.tags(params['tags']) unless params['tags'].empty?
-      documents = service_model.fetch_all
-      documents.collect { |doc| doc.document }
+    group ':app_name' do
+      params do
+        optional :include, type: String, default: 'published'
+      end
+      group ':model_name' do
+
+        desc 'get published documents'
+        params do
+          optional :tags, type: Array, default: []
+        end
+        get do
+          service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
+          params['include'] == 'published' ? service_model.published : service_model.drafts
+          service_model.tags(params['tags']) unless params['tags'].empty?
+          documents = service_model.fetch_all
+          documents.collect { |doc| doc.document }
+        end
+
+
+        desc 'get a document'
+        get ':doc_id' do
+          service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
+          params['include'] == 'published' ? service_model.published : service_model.drafts
+          doc = service_model.doc_id(params['doc_id']).fetch_one
+          error!("Document #{params['doc_id']} not found.", 404) if doc.nil?
+          doc.document
+        end
+
+
+      end
     end
 
-    desc 'get a document '
-    params do
-      optional :include, type: String, default: 'published'
-    end
-    get ':app_name/:model_name/:doc_id', requirements: { app_name: /[A-Za-z0-9\-]*/, model_name: /[A-Za-z0-9]*/, doc_id:/[A-Za-z0-9\-_]*/  } do
-      service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
-      params['include'] == 'published' ? service_model.published : service_model.drafts
-      doc = service_model.doc_id(params['doc_id']).fetch_one
-      error!("Document #{params['doc_id']} not found.", 404) if doc.nil?
-      doc.document
-    end
+
 
 
   end
