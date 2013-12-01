a4c1de0d794bc3d5819fb1070add640ff8fd0a9c
Sunit Parekh <parekh.sunit@gmail.com>
2013-12-01 17:51:39 +0530

    refactored controller using helper method to remove duplicates

--- lib/soupcms/api/model_api.rb
+++ lib/soupcms/api/model_api.rb
@@ -7,6 +7,16 @@ module SoupCMS
     prefix 'api'
     format :json
 
+    helpers do
+
+      def get_service_model
+        service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
+        params['include'] == 'published' ? service_model.published : service_model.drafts
+        service_model
+      end
+    end
+
+
     group ':app_name' do
       params do
         optional :include, type: String, default: 'published'
@@ -18,8 +28,7 @@ module SoupCMS
           optional :tags, type: Array, default: []
         end
         get do
-          service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
-          params['include'] == 'published' ? service_model.published : service_model.drafts
+          service_model = get_service_model
           service_model.tags(params['tags']) unless params['tags'].empty?
           documents = service_model.fetch_all
           documents.collect { |doc| doc.document }
@@ -28,8 +37,7 @@ module SoupCMS
 
         desc 'get a document'
         get ':doc_id' do
-          service_model = SoupCMS::Api::DataService.model(params['app_name'], params['model_name'])
-          params['include'] == 'published' ? service_model.published : service_model.drafts
+          service_model = get_service_model
           doc = service_model.doc_id(params['doc_id']).fetch_one
           error!("Document #{params['doc_id']} not found.", 404) if doc.nil?
           doc.document
