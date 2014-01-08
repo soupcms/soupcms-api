fe3f7921ac9cf68c65b0a0ab946b5919edd6812c
Sunit Parekh <parekh.sunit@gmail.com>
2014-01-08 17:43:36 +0530

    allowed ability to configure options for markdown engines

--- lib/soupcms/api/resolver/kramdown_markdown_resolver.rb
+++ lib/soupcms/api/resolver/kramdown_markdown_resolver.rb
@@ -7,9 +7,17 @@ module SoupCMS
 
       class KramdownMarkdownResolver < Base
 
+        def self.options=(options)
+          @@options = options
+        end
+
+        def options
+          @@options ||= {}
+        end
+
         def resolve(value, context)
           return value, true if value['type'] != 'markdown' || value['flavor'] != 'kramdown'
-          value['value'] = Kramdown::Document.new(value['value']).to_html
+          value['value'] = Kramdown::Document.new(value['value'], options).to_html
           return value, false
         end
 
--- lib/soupcms/api/resolver/redcarpet_markdown_resolver.rb
+++ lib/soupcms/api/resolver/redcarpet_markdown_resolver.rb
@@ -7,20 +7,37 @@ module SoupCMS
     module Resolver
 
       class CodeRayHTML < Redcarpet::Render::HTML
+        def self.options=(options)
+          @@options = options
+        end
+
+        def options
+          @@options ||= {:wrap => :div, :line_numbers => :inline, :line_number_start => 1, :tab_width => 8,
+                        :bold_every => 10, :css => :style}
+
+        end
+
         def block_code(code, language)
           return "<pre><code>#{CGI.escapeHTML(code)}</code></pre>" if language == nil || language == 'no-highlight'
-          opts = {:wrap => :div, :line_numbers => :inline, :line_number_start => 1, :tab_width => 8,
-                  :bold_every => 10, :css => :style}
-          CodeRay.scan(code, language.to_sym).html(opts).chomp << "\n"
+          CodeRay.scan(code, language.to_sym).html(options).chomp << "\n"
         end
       end
 
 
       class RedcarpetMarkdownResolver < Base
 
+        def self.options=(options)
+          @@options = options
+        end
+
+        def options
+          @@options ||= {fenced_code_blocks: true, tables: true, autolink: true, strikethrough: true, footnotes: true, superscript: true, highlight: true}
+
+        end
+
         def resolve(value, context)
           return value, true if value['type'] != 'markdown' || value['flavor'] != 'redcarpet'
-          markdown = Redcarpet::Markdown.new(CodeRayHTML.new(with_toc_data: true), fenced_code_blocks: true, tables: true, autolink: true, strikethrough: true, footnotes: true, superscript: true, highlight: true)
+          markdown = Redcarpet::Markdown.new(CodeRayHTML.new(with_toc_data: true), options)
           value['value'] = markdown.render(value['value'])
           return value, false
         end
